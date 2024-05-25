00a7aaf6388ca49bab2321385f414065066d1684
Ventsislav Peychev <del.busque@gmail.com>
2024-05-25 14:06:36 +0300

    Create expo app

---
+++ React-Native-Shaun/apps/myproject/App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>
+          Hello
+          <Text>Mobile</Text>
+          World !
+        </Text>
+      </View>
+
+      <View style={styles.body}>
+        <Text>happy dayssss</Text>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+    fontWeight: 'bold'
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20
+  }
+});
