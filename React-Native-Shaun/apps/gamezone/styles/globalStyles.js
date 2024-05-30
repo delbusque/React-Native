a22ae9b9def4c62de79659eaf4516b4251442e27
Ventsislav Peychev <del.busque@gmail.com>
2024-05-30 16:41:44 +0300

    Create globalStyles.js

---
+++ React-Native-Shaun/apps/gamezone/styles/globalStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        padding: 24
+    },
+    titleText: {
+        // fontFamily: 'Nunito_400Regular',
+        fontSize: 18,
+        color: '#533'
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20
+    }
+})
